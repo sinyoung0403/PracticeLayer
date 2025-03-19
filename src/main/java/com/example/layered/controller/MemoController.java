37a11fe2ab5592e465c362bac383fea8d373ba14
sinyoung0403 <tlstjs753@gmail.com>
2025-03-19 16:52:01 +0900

    # practice Layer

---
+++ src/main/java/com/example/layered/controller/MemoController.java
@@ -0,0 +1,69 @@
+package com.example.layered.controller;
+
+import com.example.layered.dto.MemoRequestDto;
+import com.example.layered.dto.MemoResponseDto;
+import com.example.layered.entity.Memo;
+import com.example.layered.service.MemoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/memos")
+public class MemoController {
+
+  private final MemoService memoService;
+
+  /**
+   * 생성자 주입
+   * 클래스가 필요로 하는 의존성을 생성자를 통해 전달하는 방식
+   *
+   * @param memoService @Service로 등록된 MemoService 구현체인 Impl
+   */
+  public MemoController(MemoService memoService) {
+    this.memoService = memoService;
+  }
+
+
+  @PostMapping
+  public ResponseEntity<MemoResponseDto> createMemo(@RequestBody MemoRequestDto dto) {
+    return new ResponseEntity<>(memoService.saveMemo(dto), HttpStatus.CREATED);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<MemoResponseDto>> findAllMemos() {
+    return new ResponseEntity<>(memoService.findAllMemos(), HttpStatus.OK);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<MemoResponseDto> findMemoById(@PathVariable Long id) {
+    return new ResponseEntity<>(memoService.findMemoById(id), HttpStatus.OK);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<MemoResponseDto> updateMemo(
+          @PathVariable Long id,
+          @RequestBody MemoRequestDto dto
+  ) {
+    return new ResponseEntity<>(memoService.updateMemo(id, dto.getTitle(), dto.getContents()), HttpStatus.OK);
+  }
+
+  @PatchMapping("/{id}")
+  public ResponseEntity<MemoResponseDto> updateTitle(
+          @PathVariable Long id,
+          @RequestBody MemoRequestDto dto
+  ) {
+    return new ResponseEntity<>(memoService.updateTitle(id, dto.getTitle(), dto.getContents()), HttpStatus.OK);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteMemo(@PathVariable Long id) {
+    memoService.deleteMemo(id);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+}
--- src/main/java/com/example/layered/entity/Memo.java
+++ src/main/java/com/example/layered/entity/Memo.java
@@ -1,5 +1,33 @@
 package com.example.layered.entity;
 
-public class Memo {
+import com.example.layered.dto.MemoRequestDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@AllArgsConstructor
+public class Memo {
+  // filed 를 정의한다
+  @Setter
+  private Long id;
+  private String title;
+  private String contents;
+
+  // title 과 contents 만 받을 생성자
+  public Memo(String title, String contents) {
+    this.title = title;
+    this.contents = contents;
+  }
+
+  // 전체 Update
+  public void update(String title, String contents) {
+    this.title = title;
+    this.contents = contents;
+  }
+
+  // 부분 Update
+  public void updateTitle(String title) {
+    this.title = title;
+  }
 }
